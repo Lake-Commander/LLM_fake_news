6d1c8861fccd5051bdae4e1bb462d5f1620ae8c0
Ephraim Elakhe <86500924+Lake-Commander@users.noreply.github.com>
2025-08-06 12:50:12 +0100

    retrain model

--- Dev/1.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')          # Sentence + word tokenizer
-nltk.download('stopwords')      # Common English stopwords
-nltk.download('wordnet')        # Lemmatization dictionary
-nltk.download('omw-1.4')        # Lemmatizer multilingual support
-nltk.download('averaged_perceptron_tagger')  # Optional, for POS tagging
-nltk.download('punkt_tab')
